222d30005032bfc3c2eacbf3eae013ad58427c78
黄文 <wen.huang@hualongdata.com>
2018-06-12 11:03:26 +0800

    bug fixed

---
+++ client/utils/axios.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+axios.default.timeout = 5000
+
+export function request(config) {
+  return axios(config).then((response) => {
+    if (response.data.errCode && response.data.errCode !== 0) {
+      console.error(response.data)
+      return
+    }
+    return Promise.resolve(response)
+  }).catch((e) => {
+    return Promise.reject(e)
+  })
+}
