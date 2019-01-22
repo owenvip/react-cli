ba7d4580ac50c050f0cb2d89a1bf68d6de0fc92a
黄文 <wen.huang@hualongdata.com>
2019-01-22 14:46:11 +0800

    update

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: {
-    'postcss-cssnext': {},
-    'css-declaration-sorter': {
-      order: 'concentric-css'
-    },
-    'css-mqpacker': {}
-  }
-}
--- src/store/store.js
+++
@@ -1,7 +0,0 @@
-import testState from './testState';
-
-const stores = {
-  testState,
-}
-
-export default stores
